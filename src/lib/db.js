0be98b6036ba644c89a0769bfaf8ad2d9cdc2e6e
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2022-07-24 01:28:04 +0530

    Testing DB Connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const uri = import.meta.env.MONGODB_URI;
 const options = {
@@ -9,26 +9,29 @@ const options = {
 let client;
 let clientPromise;
 
-if (!uri) {
-	throw new Error('Please add your Mongo URI to .env.local');
-}
-
-if (import.meta.env.MODE === 'development') {
-	// In development mode, use a global variable
-	// so that the value is preserved across module reloads
-	// caused by HMR (Hot Module Replacement).
-	if (!global._mongoClientPromise) {
-		client = new MongoClient(uri, options);
-		global._mongoClientPromise = client.connect();
+try {
+	if (!uri) {
+		throw new Error("Please add your Mongo URI to .env.local");
 	}
-	clientPromise = global._mongoClientPromise;
-} else {
-	// In production mode, it's best to
-	// not use a global variable.
-	client = new MongoClient(uri, options);
-	clientPromise = client.connect();
-}
 
+	if (import.meta.env.MODE === "development") {
+		// In development mode, use a global variable
+		// so that the value is preserved across module reloads
+		// caused by HMR (Hot Module Replacement).
+		if (!global._mongoClientPromise) {
+			client = new MongoClient(uri, options);
+			global._mongoClientPromise = client.connect();
+		}
+		clientPromise = global._mongoClientPromise;
+	} else {
+		// In production mode, it's best to
+		// not use a global variable.
+		client = new MongoClient(uri, options);
+		clientPromise = client.connect();
+	}
+} catch (err) {
+	console.error(err);
+}
 // Export a module-scoped MongoClient promise.
 // By doing this in a separate module,
 // the client can be shared across functions.
